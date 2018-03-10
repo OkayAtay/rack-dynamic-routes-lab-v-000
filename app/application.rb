914831cfb64f00eda6d016731b8b55540118c454
Araba Taylor <araba.taylor20@gmail.com>
2018-03-10 00:44:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,5 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+        @@items.each do |item|
+          resp.write "#{item.price}\n"
+        end
   end
 end
